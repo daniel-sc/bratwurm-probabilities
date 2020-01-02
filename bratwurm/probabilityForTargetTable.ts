c69403e4d49276df6b9f449d435760bcce109b96
Daniel Schreiber <daniel-schreiber@gmx.de>
2020-01-02 14:52:46 +0100

    prob table

--- bratwurm/probabilityForTargetTable.ts
+++ bratwurm/probabilityForTargetTable.ts
@@ -1,16 +1,19 @@
-import {BratwurmState} from './bratwurmState';
-import {prob} from './bratwurmFunctions';
+import {prob, TOTAL_DICES} from './bratwurmFunctions';
 import * as math from 'mathjs';
-import {Fraction, fraction} from 'mathjs';
+import {Fraction, larger, sum} from 'mathjs';
+import {getAllThrows} from '../dice/throw';
 
 console.debug = () => null;
 
-const startState: BratwurmState = {
-    thrown: {diceCount: [0, 0, 0, 0, 0, 0], probability: fraction(1) as Fraction},
-    fehlWurf: false
-};
+const targets = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
+const caches = new Map<number, Map<string, Fraction>>();
+targets.forEach(t => caches.set(t, new Map<string, Fraction>()));
 
-
-for (let target = 21; target <= 36; target++) {
-    console.log(`p(${target}):`, math.string(prob(target, startState)));
+for (let diceCount = 0; diceCount < TOTAL_DICES; diceCount++) {
+    for (const t of getAllThrows(diceCount)) {
+        const probs = targets.map(target => prob(target, {thrown: t, fehlWurf: false}, caches.get(target)) as Fraction);
+        if (larger(sum(...probs), 0)) { // only non-trivial results
+            console.log(`situation=${t.diceCount.join(',')}: ${targets.map((target, i) => `${target}=${math.number(probs[i])}`).join(',')}`);
+        }
+    }
 }
