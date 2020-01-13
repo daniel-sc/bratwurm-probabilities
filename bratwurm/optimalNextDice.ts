c82d8ca2c8fa851f78105c16fae30c9285bce0e6
Daniel Schreiber <daniel-schreiber@gmx.de>
2020-01-13 13:54:11 +0100

    cleanup

--- bratwurm/optimalNextDice.ts
+++ bratwurm/optimalNextDice.ts
@@ -1,5 +1,7 @@
-import {Fraction, larger, default as math, sum, MathType} from 'mathjs';
+import * as math from 'mathjs';
+import {Fraction, larger, sum} from 'mathjs';
 import {probAtLeast, probExact, situations, TARGETS, TOTAL_DICES} from './bratwurmFunctions';
+import {BratwurmState} from './bratwurmState';
 
 console.debug = () => null;
 
@@ -9,28 +11,38 @@ TARGETS.forEach(t => caches.set(t, new Map<string, Fraction>()));
 // run with cli argument 'sum' to get 'at least' semantics:
 const probFunction = process.argv.slice(2).some(arg => arg === 'sum') ? probAtLeast : probExact;
 
-const target = 21; // TODO
-const cache = new Map<string, Fraction>();
+const orderedSituations = [...situations()];
+orderedSituations.sort((a, b) => a.diceCount.join('') < b.diceCount.join('') ? -1 : 1);
 
-for (const t of situations()) {
+
+for (const t of orderedSituations) {
     const unusedDice = t.diceCount.reduce((acc, count, i) => count === 0 ? [...acc, i] : acc, []);
-    const remaining = unusedDice.length;
-    for (let diceCount = 1; diceCount <= remaining; diceCount++) {
-        const probs = unusedDice.map(dice => {
-            const newDiceCount = [...t.diceCount];
-            newDiceCount[dice] = remaining;
-            return probFunction(target, {
-                thrown: {diceCount: newDiceCount, probability: 1},
-                fehlWurf: false
-            }, cache);
-        });
+    // console.log('unused dice: ', unusedDice);
+    const remaining = TOTAL_DICES - sum(...t.diceCount);
 
-    }
-    const probs = TARGETS.map(target => probFunction(target, {
+    const originalState: BratwurmState = {
         thrown: t,
         fehlWurf: false
-    }, caches.get(target)) as Fraction);
-    if (larger(sum(...probs), 0)) { // only non-trivial results
-        console.log(`situation=${t.diceCount.join(',')}: ${TARGETS.map((target, i) => `${target}=${math.number(probs[i])}`).join(',')}`);
+    };
+    const probs = TARGETS.map(target => probFunction(target, originalState, caches.get(target)) as Fraction);
+
+    if (larger(sum(...probs), 0) && probs.some(p => larger(p, 0) && larger(1, p))) { // only non-trivial results
+        unusedDice.forEach((unusedDice: number) => {
+
+
+            for (let diceCount = 1; diceCount <= remaining; diceCount++) {
+                const newDiceCount = [...t.diceCount];
+                newDiceCount[unusedDice] = diceCount;
+                const state: BratwurmState = {
+                    thrown: {...t, diceCount: newDiceCount},
+                    fehlWurf: false
+                };
+                const probs = TARGETS.map(target => probFunction(target, state, caches.get(target)) as Fraction);
+                if (larger(sum(...probs), 0) && probs.some(p => larger(p, 0) && larger(1, p))) { // only non-trivial results
+                    // console.log('probs: ', probs);
+                    console.log(`situation=${t.diceCount.join(',')} (${diceCount}x${unusedDice + 1}): ${TARGETS.map((target, i) => `${target}=${math.number(probs[i])}`).join(',')}`);
+                }
+            }
+        });
     }
 }
